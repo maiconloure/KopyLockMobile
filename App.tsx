3390d76e686693fca5057c3f89e776ee2a0eab60
Maicon Lourenço <maiconloure@gmail.com>
2023-02-01 22:30:45 -0300

    implements notifications

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import './src/lib/dayjs';
-import { StatusBar } from 'react-native';
+import { StatusBar, Button } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter'
 
 import { Loading } from './src/components/Loading';
 import { Routes } from './src/routes';
 import { Logs } from 'expo'
+import * as Notifications from 'expo-notifications';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => (
+    {
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false
+    }
+  )
+})
+
 Logs.enableExpoCliLogging()
 
 export default function App() {
@@ -15,6 +27,25 @@ export default function App() {
     Inter_800ExtraBold
   })
 
+  async function scheduleNotification() {
+    const trigger = new Date(Date.now())
+    trigger.setMinutes(trigger.getMinutes() + 1)
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'Olá, Tester 🥲',
+        body: 'Did you practice your habits today?'
+      },
+      trigger
+    })
+  }
+
+  async function getScheduledNotification() {
+    const schedules = await Notifications.getAllScheduledNotificationsAsync();
+    console.log(schedules)
+  }
+
+
   if (!fontsLoaded) {
     return (
       <Loading />
@@ -23,8 +54,10 @@ export default function App() {
 
   return (
     <>
+      <Button title='Send Notifications' onPress={scheduleNotification} />
+      <Button title='Schedule Notifications' onPress={getScheduledNotification} />
       <Routes />
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent  />
     </>
   );
 }
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://172.17.252.116:3333'
+  baseURL: 'http://172.30.170.242:3333'
 })
